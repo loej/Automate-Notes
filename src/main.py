d82a0572986a9c02bb333f5190e628456638d0fd
loej <joel.martinez@rutgers.edu>
2020-07-15 10:04:43 -0400

    Modified files.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-# Automating my note taking using LaTeX and Vim 
+# Automating my note taking using LaTeX and Vim
+import os
+import math
+
+
+class Notes:
+    pass
+
+
+class Vim:
+    pass
+
+
+class Automate:
+    pass

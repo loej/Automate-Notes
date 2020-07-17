206d17be34ed7707e7f89d001198aecf365c3c2c
loej <joel.martinez@rutgers.edu>
2020-07-17 17:11:33 -0400

    Implemented Prompt.

--- src/notes.py
+++ src/notes.py
@@ -13,49 +13,54 @@ class Notes:
         self.folder = folder
 
 
-class Vim:
-    pass
+def makeNewFile():
+    makeNewFile = input('To make a new file enter 1, if not press 2: ')
+    if not(makeNewFile.isdigit()):
+        while True:
+            input4 = input('Please enter a number: ')
+            if input4.isdigit():
+                break
 
 
-class Automate:
-
-    pass
+def enterNewFile():
+    enterOldFile = str(input('To access an old file, type its name: '))
+    if enterOldFile.isdigit():
+        while True:
+            input3 = input(('Please enter a name: '))
+            if not(input3.isdigit()):
+                break
+    # calls AutomateNotes()
+    autoMoteNotes()
 
 
 def printPrompt():
+    folderLst = ['calc']
+    notesObj = Notes(folderLst)
     print('Automate Notes - Using LaTeX and Vim \n')
     print('Here are your current folders: ' + str(notesObj.folder))
-    try:
-        newFolder = int(
-            input('To make a new folder for a class press input 1: '))
-        useFolder = str(
-            input('To access your previous files just type the name: '))
-    except ValueError:
-        print('Oops! that is not a valid input')
 
-    # Calling autoMateNotes function
+    makeNewFile()
+    enterNewFile()
 
 
-def autoMateNotes():
-    if printPrompt().newFolder == 1:
+def autoMoteNotes():
+    if enterNewFile == str:
         print('test')
-    else:
-        sys.exit()
 
 
-def terminalCommand():
-    # Command being used.
-    p1 = Automate('notes', '12')
-    p1.command = 'notes'
+# def terminalCommand():
+#     # # Command being used.
+#     # p1 = Automate('notes', '12')
+#     # p1.command = 'notes'
 
-    # This is going to take in the argument from running the file
-    for p1.command in sys.argv:
-        print(p1.command)
+#     # # This is going to take in the argument from running the file
+#     # for p1.command in sys.argv:
+#     #     print(p1.command)
+#     pass
 
 
 def main():
     printPrompt()
-    autoMateNotes()
 
 
 if __name__ == "__main__":
